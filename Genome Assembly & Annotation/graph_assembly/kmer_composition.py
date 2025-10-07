feec7ae8fd6510534ed894c383886c19c134a13a
emilywei4 <emily.a.wei@gmail.com>
2025-10-07 13:46:05 -0700

    Update kmer_composition.py

--- Genome Assembly & Annotation/graph_assembly/kmer_composition.py
+++ Genome Assembly & Annotation/graph_assembly/kmer_composition.py
@@ -2,9 +2,6 @@
 import sys
 from typing import List, Dict, Iterable
 
-# Please do not remove package declarations because these are used by the autograder.
-
-# Insert your kmer_composition function here, along with any subroutines you need
 def kmer_composition(text: str, k: int) -> Iterable[str]:
     """Forms the k-mer composition of a string."""
     kmers = []
